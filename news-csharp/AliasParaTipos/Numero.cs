966b53aa8eef1bf679bf4ea843e6d9d2a99f1bb7
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-09-26 07:06:46 -0300

    feat: Add news csharp 12

---
+++ news-csharp/AliasParaTipos/Numero.cs
@@ -0,0 +1,24 @@
+﻿namespace news_csharp.AliasParaTipos
+{
+    using Altura = (string, int);
+    using ListaInteiros = int[];
+    using Inteiro = int?;
+
+    public class AliasService
+    {
+        public Altura RetornarAltura()
+        {
+            return ("Altura", 180); // Retorna uma tupla com o nome e valor
+        }
+
+        public ListaInteiros RetornarInteiros()
+        {
+            return [10, 20, 30, 40]; // Retorna um array de inteiros
+        }
+
+        public Inteiro GetDatabaseIntValue()
+        {
+            return 100; // Retorna um valor inteiro nulável
+        }
+    }
+}
